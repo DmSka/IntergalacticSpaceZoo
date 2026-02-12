ad976f60b37850921c037b4cbf5a88a63051e563
DmSka <40703147+DmSka@users.noreply.github.com>
2026-02-11 23:19:11 -0500

    version0.5

--- Assets/Scripts/Galaxy.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Galaxy : MonoBehaviour
-{
-    //stores collection of currently generated planets
-    public List<PlanetData> currentPlanets = new List<PlanetData>();
-}
